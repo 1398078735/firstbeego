b1b8fdccdcdc83f2b7ceda9fb0ad447368ddb170
tengyuanqianhua5 <1398078735@qq.com>
2020-09-25 11:46:44 +0800

    json解析

--- controllers/default.go
+++ controllers/default.go
@@ -1,8 +1,11 @@
 package controllers
 
 import (
+	"beegonew0604/models"
+	"encoding/json"
 	"fmt"
 	"github.com/astaxie/beego"
+	"io/ioutil"
 )
 
 type MainController struct {
@@ -10,6 +13,16 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
+	//第二种方法name:=c.GetString("name")
+	//age1,err:=c.GetInt("age")
+
+	//JSON格式提交:{
+	//    "name" : "tengyuan",
+	//    "age" : 18,
+	//    "sex" ："famale"
+	//}
+	//后台处理
+
 	//获取get类型请求的请求参数
 	name := c.Ctx.Input.Query("name")
 	fmt.Println("名字为:",name)
@@ -18,12 +31,13 @@ func (c *MainController) Get() {
 	sex := c.Ctx.Input.Query("sex")
 	fmt.Println("性别是:",sex)
 
-	if name != "admin" || age != "18" {
+	if name != "藤原千花" || age != "18" {
 		c.Ctx.ResponseWriter.Write([]byte("数据验证错误"))
 		return
 	}
 
 	c.Ctx.ResponseWriter.Write([]byte("数据验证成功"))
+	//c.ctx.writeString()直接写入字符串
 
 	c.Data["Website"] = "wwww.baidu.com"
 	c.Data["Email"] = "hanhan@qq.com"
@@ -34,15 +48,32 @@ func (c *MainController) Get() {
 func (c *MainController) Post(){
 	//request 请求,response响应
 	fmt.Println("post请求")
-	user := c.Ctx.Request.FormValue("user")
+	/*user := c.Ctx.Request.FormValue("user")
 	fmt.Println("用户名为:",user)
 	psd := c.Ctx.Request.FormValue("psd")
 	fmt.Println("密码是:",psd)
 	//与固定值比较
-	if user != "admin"  ||  psd != "123456"{
+	if user != "藤原千花"  ||  psd != "123456"{
 		//失败页面
 		c.Ctx.ResponseWriter.Write([]byte("对不起数据不正确"))
 	}else {
 		c.Ctx.ResponseWriter.Write([]byte("恭喜你,数据正确"))
+	}*/
+	//后台处理
+	body := c.Ctx.Request.Body
+	dataByes,err:=ioutil.ReadAll(body)
+	if err != nil {
+		c.Ctx.WriteString("数据接收失败")
+		return
 	}
+	//json包解析
+	var person models.Person
+	json.Unmarshal(dataByes,&person)
+	if err != nil {
+		c.Ctx.WriteString("数据解析失败")
+		return
+	}
+	fmt.Println("用户名",person.Name,"年龄",person.Age,"性别",person.Sex)
+	c.Ctx.WriteString("用户名:"+person.Name)
+
 }
--- main.go
+++ main.go
@@ -2,10 +2,37 @@ package main
 
 import (
 	_ "beegonew0604/routers"
+	"database/sql"
+	"fmt"
 	"github.com/astaxie/beego"
 )
 
 func main() {
+	//解析配置选项
+	config :=beego.AppConfig //定义config变量,接收并赋值为全局配置变量
+	//获取配置选项
+	appName := config.String("appname")
+	fmt.Println("项目应用名称",appName)
+	port,err := config.Int("httpport")
+	if err != nil {
+		panic("项目配置信息解析错误")
+	}
+	fmt.Println("应用的监听端口",port)
+
+	dbdriver := config.String("db_driver")
+	dbuser := config.String("db_user")
+	dbpassword:= config.String("db_password")
+	dbip := config.String("db_ip")
+	dbname := config.String("db_name")
+
+	db,err:=sql.Open(dbdriver,dbuser+":"+dbpassword+"@tcp("+dbip+")/"+dbname+"?charset=utf8")
+	if err != nil {
+		panic("数据连接打开失败")
+	}
+	fmt.Println(db)
+
 	beego.Run()
+	//beego框架的配置文件操作:
+	//全局应用配置结构体变量:beego.appconfig
 }
 
